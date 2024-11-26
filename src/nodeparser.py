da90b2cbab687419d47d1728ee878130be698165
El-fonto <laotrafuente880@gmail.com>
2024-11-26 17:58:48 -0600

    clean smthng

--- src/nodeparser.py
+++ src/nodeparser.py
@@ -2,20 +2,28 @@ from textnode import TextType, TextNode
 
 
 def main():
-    node = TextNode("Before *bolded text* after", TextType.TEXT)
+    bold_node = TextNode("Before **bolded text** after", TextType.TEXT)
+    italic_node = TextNode("Before *italized text* after", TextType.TEXT)
+    code_node = TextNode("Before `code text` after", TextType.TEXT)
 
-    old_nodes = [
-        node,
-        # TextNode("Before **bolded ** after", TextType.TEXT),
-        # TextNode("Before `code` after", TextType.TEXT),
-    ]
+    bold_nodes = [bold_node]
+    italic_nodes = [italic_node]
+    code_nodes = [code_node]
 
-    print(f"giving the old_nodes as: {old_nodes}")
-    print("================== I GET =================")
-    print(f"{split_node_delimiter(old_nodes, "*", TextType.BOLD)}")
+    print(f"{split_node_delimiter(bold_nodes, "**", TextType.BOLD)}")
+    print()
+    print(f"{split_node_delimiter(italic_nodes, "*", TextType.ITALIC)}")
+    print()
+    print(f"{split_node_delimiter(code_nodes, "`", TextType.CODE)}")
 
 
 def split_node_delimiter(old_nodes, delimiter, text_type):
+    DELIMITER_MAP = {
+        "**": TextType.BOLD,
+        "*": TextType.ITALIC,
+        "`": TextType.CODE,
+    }
+
     def splitter(old_nodes):
         new_nodes = []
         for old_node in old_nodes:
@@ -36,7 +44,8 @@ def split_node_delimiter(old_nodes, delimiter, text_type):
                 raise ValueError("Invalid markdown syntax")
 
             middle, after = rest.split(delimiter, 1)
-            new_nodes.append(TextNode(middle, text_type))
+            if DELIMITER_MAP.get(delimiter) == text_type:
+                new_nodes.append(TextNode(middle, text_type))
 
             if after:
                 new_nodes.extend(splitter([TextNode(after, TextType.TEXT)]))
