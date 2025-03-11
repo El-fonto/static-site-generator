d179416d353f403108fbb3aa410fa6bd58e277af
El-fonto <laotrafuente880@gmail.com>
2025-03-11 08:40:08 -0600

    extract links/images functions done and tested, split_images/links started

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 
@@ -31,3 +32,64 @@ def split_nodes_delimiter(
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        # extract the list of tuples
+        link_node_tup = extract_markdown_links(old_node.text)
+
+        # base case to exit if no links/images in node
+        if len(link_node_tup) == 0:
+            new_nodes.append([old_node])
+
+        # need to count how many links are there
+        #
+        # probably, tup indexes will be a variable; i.e. `i` and `i+1`, to account the number of links
+
+        if len(link_node_tup) != 0:
+            new_nodes.append(
+                TextNode(
+                    text=link_node_tup[0][0],
+                    text_type=TextType.LINK,
+                    url=link_node_tup[0][1],
+                )
+            )
+
+    return new_nodes
+
+
+def main():
+    node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+        TextType.TEXT,
+    )
+    node_no_links = TextNode("text with no links", TextType.TEXT)
+
+    new_nodes = split_nodes_link([node, node_no_links])
+
+    print("EXTRACTED: ", end="")
+    print(extract_markdown_links(node.text))
+    print("SPLITTED: ", end="")
+    print(new_nodes)
+
+
+main()
+# def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+#    new_nodes = []
+#    for old_node in old_nodes:
+#        # if old_node.text_type !=
+#        if extract_markdown_images(old_node.text):
+#            new_nodes.append(old_node)
+#    return new_nodes
--- src/textnode.py
+++ src/textnode.py
@@ -26,8 +26,8 @@ class TextNode:
 
     def __repr__(self) -> str:
         if self.url is None:
-            return f"TextNode({self.text}, {self.text_type.value})"
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+            return f"TextNode({self.text}, {self.text_type})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
 def text_node_to_html_node(textnode: TextNode) -> LeafNode:
