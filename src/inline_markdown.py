69b21475c51e0bf37556b6f4426ba7059843c80e
El-fonto <laotrafuente880@gmail.com>
2025-03-13 12:54:36 -0600

    text to textnodes function started

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -120,3 +120,14 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
         if remaining_text != "":
             new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    pass
+
+
+def main():
+    example_text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+
+main()
