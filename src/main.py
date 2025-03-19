65ad728b87b7c7ae9e9dd575fd161817991a4a2e
El-fonto <laotrafuente880@gmail.com>
2025-03-19 08:00:14 -0600

    started testing with main file, with std library utils

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
 import os
+import shutil
 
 # TODO: recursive functions that.
 
 # check if path exists
+#
+#
+
+
+def main():
+    path = "~/workspace/github.com/El-fonto/static-site-generator/public/"
+
+    print(os.path.isfile(path))
+    # first: delete contents of public
+    check = os.path.exists(path)
+    print(check)
+
+    if check:
+        shutil.rmtree(path)
+        print(check)
+    #
+
+
+main()
