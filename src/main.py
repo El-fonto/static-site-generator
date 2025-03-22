e5b34fb131213be71e8d479368001955a8cdb9d6
El-fonto <laotrafuente880@gmail.com>
2025-03-22 13:31:12 -0600

    handling of destination's filetree done. Copying not finished, recursion is missing

--- src/main.py
+++ src/main.py
@@ -9,18 +9,35 @@ import shutil
 
 
 def main():
-    path = "~/workspace/github.com/El-fonto/static-site-generator/public/"
+    PUBLIC_PATH = (
+        "/home/fonto/workspace/github.com/El-fonto/static-site-generator/public/"
+    )
+    STATIC_PATH = (
+        "/home/fonto/workspace/github.com/El-fonto/static-site-generator/static/"
+    )
 
-    print(os.path.isfile(path))
-    # first: delete contents of public
-    check = os.path.exists(path)
-    print(check)
+    # handling destination directory
+    public_exists = os.path.exists(PUBLIC_PATH)
+    if not public_exists:
+        print(f"=== {PUBLIC_PATH} === didn't exist")
+        os.mkdir(PUBLIC_PATH)
+        print("path made")
+    if public_exists:
+        print(f"=== {PUBLIC_PATH} === DELETED")
+        # delete contents of path
+        shutil.rmtree(PUBLIC_PATH)
+    #
+    # traverse directory
+    static_content = os.listdir(STATIC_PATH)
 
-    if check:
-        shutil.rmtree(path)
-        print(check)
-    # new things
-    # new things 2
+    print(static_content)
+    for file in static_content:
+        if os.path.isfile(f"{STATIC_PATH}{file}"):
+            print(f"match {file}")
+            shutil.copy(STATIC_PATH, PUBLIC_PATH)
+        if os.path.isdir(f"{STATIC_PATH}{file}"):
+            # recursion needs work
+            print(f"this is a directory: {file}")
 
 
 main()
