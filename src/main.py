b50ddce7e073ae7dffb85bf762a3f8700688936b
El-fonto <laotrafuente880@gmail.com>
2025-03-23 14:40:12 -0600

    recursive function started

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ def main():
     STATIC_PATH = (
         "/home/fonto/workspace/github.com/El-fonto/static-site-generator/static/"
     )
-
     # handling destination directory
     public_exists = os.path.exists(PUBLIC_PATH)
     if not public_exists:
@@ -31,13 +30,30 @@ def main():
     static_content = os.listdir(STATIC_PATH)
 
     print(static_content)
+
     for file in static_content:
         if os.path.isfile(f"{STATIC_PATH}{file}"):
             print(f"match {file}")
-            shutil.copy(STATIC_PATH, PUBLIC_PATH)
+            # shutil.copy(STATIC_PATH, PUBLIC_PATH)
         if os.path.isdir(f"{STATIC_PATH}{file}"):
             # recursion needs work
             print(f"this is a directory: {file}")
+
+
+def copy_file(origin_path, destination_path):
+    if len(os.listdir(origin_path)) == 0:
+        return
+    origin_content = os.listdir(origin_path)
+
+    for file in origin_content:
+        origin_file_path = f"{origin_path}{file}"
+        destination_file_path = f"{destination_path}{file}"
+
+        if os.path.isfile(origin_file_path):
+            shutil.copy(origin_file_path, destination_file_path)
+        elif os.path.isdir(origin_path):
+            os.mkdir(file)
+            copy_file(origin_file_path, destination_file_path)
 
 
 main()
