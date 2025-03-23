67e7c84bc5f46e6deb4469e40fcb9d43b237062e
El-fonto <laotrafuente880@gmail.com>
2025-03-23 17:07:27 -0600

    copy static function done

--- src/main.py
+++ src/main.py
@@ -1,59 +1,45 @@
 import os
 import shutil
 
-# TODO: recursive functions that.
-
-# check if path exists
-#
-#
-
 
 def main():
-    PUBLIC_PATH = (
-        "/home/fonto/workspace/github.com/El-fonto/static-site-generator/public/"
-    )
-    STATIC_PATH = (
-        "/home/fonto/workspace/github.com/El-fonto/static-site-generator/static/"
-    )
-    # handling destination directory
-    public_exists = os.path.exists(PUBLIC_PATH)
-    if not public_exists:
-        print(f"=== {PUBLIC_PATH} === didn't exist")
-        os.mkdir(PUBLIC_PATH)
-        print("path made")
-    if public_exists:
-        print(f"=== {PUBLIC_PATH} === DELETED")
-        # delete contents of path
-        shutil.rmtree(PUBLIC_PATH)
-    #
+    PUBLIC_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "public")
+    STATIC_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static")
+
     # traverse directory
-    static_content = os.listdir(STATIC_PATH)
-
-    print(static_content)
-
-    for file in static_content:
-        if os.path.isfile(f"{STATIC_PATH}{file}"):
-            print(f"match {file}")
-            # shutil.copy(STATIC_PATH, PUBLIC_PATH)
-        if os.path.isdir(f"{STATIC_PATH}{file}"):
-            # recursion needs work
-            print(f"this is a directory: {file}")
+    copy_file(STATIC_PATH, PUBLIC_PATH)
 
 
 def copy_file(origin_path, destination_path):
+    # handling destination directory
+    if not os.path.exists(destination_path):
+        try:
+            os.mkdir(destination_path)
+            print(f"Directory created: {destination_path}")
+        except OSError as e:
+            print(f"Error creating directory {destination_path}: {e}")
+            return
+
     if len(os.listdir(origin_path)) == 0:
+        print("end of traversing")
         return
     origin_content = os.listdir(origin_path)
 
     for file in origin_content:
-        origin_file_path = f"{origin_path}{file}"
-        destination_file_path = f"{destination_path}{file}"
+        or_full_path = os.path.join(origin_path, file)
+        dest_full_path = os.path.join(destination_path, file)
 
-        if os.path.isfile(origin_file_path):
-            shutil.copy(origin_file_path, destination_file_path)
-        elif os.path.isdir(origin_path):
-            os.mkdir(file)
-            copy_file(origin_file_path, destination_file_path)
+        if os.path.isfile(or_full_path):
+            shutil.copy(or_full_path, dest_full_path)
+        elif os.path.isdir(or_full_path):
+            try:
+                os.mkdir(dest_full_path)
+                print(f"Directory created successfully: {dest_full_path}")
+                copy_file(or_full_path, dest_full_path)
+            except FileExistsError:
+                print(f"Directory already exists: {dest_full_path}")
+            except OSError as e:
+                print(f"Error creating directory {dest_full_path}: {e}")
 
 
 main()
