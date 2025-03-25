6f6e4ba980b3adf975e7268ddc06ea09ab7173cf
El-fonto <laotrafuente880@gmail.com>
2025-03-25 14:24:59 -0600

    generate recursive tdone. Working on script to deploy on githubsites

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,19 +3,19 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def extract_title(markdown: str):
-    markdown_lines = markdown.split("\n")
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for file in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, file)
+        dest_path = os.path.join(dest_dir_path, file)
 
-    if not markdown_lines:
-        raise ValueError("There's no h1 header")
-
-    for line in markdown_lines:
-        if line.startswith("# "):
-            return line[2:].strip()
-    raise ValueError("There's no h1 header")
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, str(dest_path), basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as c, open(template_path, "r") as t:
@@ -26,30 +26,27 @@ def generate_page(from_path, template_path, dest_path):
     html_content = content_node.to_html()
 
     title = extract_title(markdown)
-    title_added = template_html.replace(r"{{ Title }}", title)
-    contented_added = title_added.replace(r"{{ Content }}", html_content)
+    template = template_html.replace(r"{{ Title }}", title)
+    template = template.replace(r"{{ Content }}", html_content)
+    template = template.replace(r'href="/', f'href="{basepath}')
+    template = template.replace(r'src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir, exist_ok=True)
 
     with open(dest_path, "w") as d:
-        d.write(contented_added)
+        d.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    content_dirs = os.listdir(dir_path_content)
-    for file in content_dirs:
-        content_full_path = os.path.join(dir_path_content, file)
-        destination_full_path = os.path.join(dest_dir_path, file)
+def extract_title(markdown: str):
+    markdown_lines = markdown.split("\n")
 
-        if os.path.isfile(content_full_path):
-            content_path = Path(content_full_path)
-            if content_path.suffix.lower() == "md":
-                dest_path = Path(destination_full_path)
-                dest_path = dest_path.with_suffix(".html")
-                generate_page(content_full_path, template_path, str(dest_path))
-        else:
-            generate_pages_recursive(
-                content_full_path, template_path, destination_full_path
-            )
+    if not markdown_lines:
+        raise ValueError("There's no h1 header")
+
+    for line in markdown_lines:
+        line = line.lstrip()
+        if line.startswith("# "):
+            return line[2:].strip()
+    raise ValueError("There's no h1 header")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import shutil
 from copystatic import copy_file_recursive
 from generate_page import generate_pages_recursive
+
 
 PUBLIC_PATH = "./public"
 STATIC_PATH = "./static"
@@ -10,6 +12,11 @@ TEMPLATE_PATH = "./template.html"
 
 
 def main():
+    if len(sys.argv) > 1:
+        BASE_PATH = sys.argv[1]
+    else:
+        BASE_PATH = "/"
+
     print("Deleting public directory...")
     if os.path.exists(PUBLIC_PATH):
         shutil.rmtree(PUBLIC_PATH)
@@ -18,11 +25,7 @@ def main():
     copy_file_recursive(STATIC_PATH, PUBLIC_PATH)
 
     print("Generating pages...")
-    generate_pages_recursive(
-        os.path.join(CONTENT_PATH, "index.md"),
-        TEMPLATE_PATH,
-        os.path.join(PUBLIC_PATH, "index.html"),
-    )
+    generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, PUBLIC_PATH, BASE_PATH)
 
 
 main()
