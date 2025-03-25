0cae27e969c42c813db412a75f92aed1b3fc45ba
El-fonto <laotrafuente880@gmail.com>
2025-03-25 14:28:18 -0600

    docs created to deploy, public dir removed

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from copystatic import copy_file_recursive
 from generate_page import generate_pages_recursive
 
 
-PUBLIC_PATH = "./public"
+DOCS_PATH = "./docs"
 STATIC_PATH = "./static"
 CONTENT_PATH = "./content"
 TEMPLATE_PATH = "./template.html"
@@ -17,15 +17,15 @@ def main():
     else:
         BASE_PATH = "/"
 
-    print("Deleting public directory...")
-    if os.path.exists(PUBLIC_PATH):
-        shutil.rmtree(PUBLIC_PATH)
+    print("Deleting docs directory...")
+    if os.path.exists(DOCS_PATH):
+        shutil.rmtree(DOCS_PATH)
 
-    print("Copying static files to public directory")
-    copy_file_recursive(STATIC_PATH, PUBLIC_PATH)
+    print("Copying static files to docs directory")
+    copy_file_recursive(STATIC_PATH, DOCS_PATH)
 
     print("Generating pages...")
-    generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, PUBLIC_PATH, BASE_PATH)
+    generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, DOCS_PATH, BASE_PATH)
 
 
 main()
